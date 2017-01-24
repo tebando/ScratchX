257ba9986420d63524f46cb2809585860a55b8a1
shintakeda <shintakeda3330@yahoo.co.jp>
2017-01-25 03:47:44 +0900

    debug test
    debug test

--- scratchx.js
+++ scratchx.js
@@ -246,7 +246,7 @@ var descriptor = {
     /* 照度 */
     ["R", "%m.menu の照度", "light", ip_key[0]],
     /* 超音波距離センサ */
-    ["R", "%m.distance_menu の超音波距離センサ", "distance", "A"]
+    ["R", "%m.distance_menu の超音波距離センサ", "distance", "鳴門"]
   ],
   "menus": {
      menu: ip_key,
