0a2a48e6d6bc804a63832bc67eeb3b5c83a53b26
shintakeda <shintakeda3330@yahoo.co.jp>
2017-02-04 16:16:14 +0900

    colon
    colon

--- Wio_Node.js
+++ Wio_Node.js
@@ -28,13 +28,36 @@
           }
     });
   };
+  /* 4-Digit Display Colon */
+  ext.fddc = function(dis) {
+    var dis_val = 0
+    if (dis == '表示する') {
+      dis_val = 1;
+    }
+
+    $.ajax({
+          url:'https://cn.wio.seeed.io/v1/node/Grove4DigitUART0/display_point/' + dis_val + '?access_token='+token,
+          type:'POST',
+          timeout:5000,
+          dataType:'json',
+          success: function(data){
+            console.log(data.result);
+          },
+          error : function(){
+            console.log('error');
+          }
+    });
+  };
 
 var descriptor = {
   "blocks": [
     /* 4-Digit Display */
-    [" ", "%n 番目に%n を表示する", "fdd", "",""]
+    [" ", "数字表示機の%n 番目に%n を表示する", "fdd", "",""],
+    [" ", "数字表示機の:を %m.display", "fddc", "表示する"]
   ],
-  "menus": {}
+  "menus": {
+     display:["表示する","表示しない"]
+  }
 };
 
 ScratchExtensions.register("ブロック群のタイトル", descriptor, ext);
